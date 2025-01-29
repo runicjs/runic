f835fcf94659e840bdf822b4256cb85082a72628
Will Bowers <william.bowers@gmail.com>
2025-01-28 17:45:07 -0800

    Add tailwind

--- src/example-app/App.tsx
+++ src/example-app/App.tsx
@@ -8,15 +8,15 @@ function App() {
   const count = useStore(countStore, (state) => state.count);
 
   return (
-    <>
-      <h1>runic</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold">runic</h1>
 
-      <div className="card">
+      <div className="p-8 flex flex-row gap-4 items-center">
         <button onClick={decrement}>-</button>
-        <span className="count">{count}</span>
+        <span className="text-6xl">{count}</span>
         <button onClick={increment}>+</button>
       </div>
-    </>
+    </div>
   );
 }
 
